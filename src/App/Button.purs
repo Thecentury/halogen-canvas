14abbea4fc7558363f5066006f1be6699125f9e2
Mikhail Brinchuk <thecentury@gmail.com>
2024-02-12 00:06:12 +0100

    Drawing

--- src/App/Button.purs
+++ src/App/Button.purs
@@ -6,14 +6,12 @@ import Color (black)
 import CSS (border, px, solid)
 import Data.Array as Array
 import Data.Int as Int
-import Data.List (range)
-import Data.Maybe (Maybe(..))
-import Data.Number (pi)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Traversable (traverse_)
 import Data.Typelevel.Num (D2)
 import Data.Vec (Vec, vec2)
 import Effect (Effect)
-import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Class (class MonadEffect)
 import Effect.Console as Console
 import Graphics.Canvas (Context2D)
 import Graphics.Canvas as GCanvas
@@ -22,46 +20,94 @@ import Halogen.Canvas (Input)
 import Halogen.Canvas as Canvas
 import Halogen.Canvas.Renderer (Renderer)
 import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
 import Halogen.HTML.Events as HE
 import Type.Proxy (Proxy(..))
 import Unsafe.Coerce (unsafeCoerce)
 import Web.HTML.HTMLCanvasElement (HTMLCanvasElement)
 import Web.UIEvent.MouseEvent (MouseEvent)
-import Web.UIEvent.MouseEvent (clientX, clientY, screenX, screenY) as Mouse
+import Web.UIEvent.MouseEvent (altKey) as Mouse
 import App.MouseEvent (offsetX, offsetY) as Mouse
-import CSS.Geometry (height, width)
+import CSS.Geometry (height, width) as CSS
 import Halogen.HTML.CSS (style)
+import Data.Int (rem)
 
-type State
-  = { count :: Int }
+data Cell =
+  Empty
+  | Concrete
+
+type State = {
+  cells :: Array Cell
+}
+
+type Coord = { x :: Int, y :: Int }
+
+type WithCoord a = { coord :: Coord, cell :: a }
+
+coordIndex :: Coord -> Int
+coordIndex { x, y } = y * worldWidth + x
+
+indexToCoord :: Int -> Coord
+indexToCoord i = { x: i `rem` worldWidth, y: i / worldWidth }
+
+cellsWithCoordinates :: State -> Array (WithCoord Cell)
+cellsWithCoordinates world =
+  Array.zipWith (\i cell -> { coord: indexToCoord i, cell }) (Array.range 0 (Array.length world.cells)) world.cells
+
+cellAt :: Coord -> State -> Maybe Cell
+cellAt coord world = Array.index world.cells (coordIndex coord)
+
+setCell :: Coord -> Cell -> State -> State
+setCell coord cell world = world { cells = fromMaybe world.cells $ Array.updateAt (coordIndex coord) cell world.cells }
+
+mousePosToCoord :: MouseEvent -> Coord
+mousePosToCoord e =
+  let
+    x = Mouse.offsetX e / pixelSize
+    y = Mouse.offsetY e / pixelSize
+  in
+    { x, y }
+
+pixelWidth :: Number
+pixelWidth = 300.0
+
+pixelHeight :: Number
+pixelHeight = 300.0
+
+pixelSize :: Int
+pixelSize = 3
+
+worldWidth :: Int
+worldWidth = Int.round pixelWidth / pixelSize
+
+worldHeight :: Int
+worldHeight = Int.round pixelHeight / pixelSize
 
 data Action =
-   Increment
- | MouseMove MouseEvent
+  MouseMove MouseEvent
 
 _canvas = Proxy :: Proxy "canvas"
 
 component :: forall q i o m. MonadEffect m => H.Component q i o m
 component =
   H.mkComponent
-    { initialState: \_ -> { count: 0 }
-    , render
-    , eval: H.mkEval H.defaultEval { handleAction = handleAction }
+    { initialState: \_ ->
+      { cells: Array.replicate (worldWidth * worldHeight) Empty },
+      render,
+      eval: H.mkEval H.defaultEval { handleAction = handleAction }
     }
 
 type Slots = ( canvas :: forall query. H.Slot query Void Unit )
 
 render :: forall m. MonadEffect m => State -> H.ComponentHTML Action Slots m
-render _state =
+render state =
   HH.div_
     [
       HH.div
         [
           HE.onMouseMove MouseMove,
           style $ do
-            width (px 300.0)
-            height (px 300.0)
+            CSS.width (px pixelWidth)
+            CSS.height (px pixelHeight)
         ]
         [
           HH.slot_ _canvas unit (Canvas.mkComponent cfg) input
@@ -69,66 +115,57 @@ render _state =
     ]
   where
     cfg = { renderer }
-    input :: Input (Array Picture)
+    input :: Input State
     input =
-      { picture : rectangles
+      { picture : state
       , css : Just (border solid (px 0.5) black)
-      , size : vec2 w h
+      , size : vec2 pixelWidth pixelHeight
       }
-    -- 100 rectangles moving from left to right:
-    rectangles :: Array Picture
-    rectangles =
-      Array.fromFoldable
-      $ (\i -> Rect { x: (Int.toNumber i) * 10.0 + 2.0, y: 10.0, width: 6.0, height: 6.0 })
-      <$> range 1 30
-
-    w = 300.0
-    h = 300.0
 
 data Picture
   = Rect { x :: Number, y :: Number, width :: Number, height :: Number }
   | Circle { x :: Number, y :: Number, radius :: Number }
 
-renderer :: Renderer GCanvas.Context2D (Array Picture)
+coordToRect :: Coord -> GCanvas.Rectangle
+coordToRect { x, y } =
+  { x: Int.toNumber x * size,
+    y: Int.toNumber y * size,
+    width: size,
+    height: size
+  }
+  where
+    size = Int.toNumber pixelSize
+
+
+renderer :: Renderer GCanvas.Context2D State
 renderer =
-  { init, render : render2, onResize }
+  { init, render : renderWorld, onResize }
   where
     init :: Vec D2 Number -> HTMLCanvasElement -> Effect (Maybe Context2D)
     init _size canvasElem =
       map Just $ GCanvas.getContext2D $ unsafeCoerce canvasElem
 
-    render2 :: Context2D -> Array Picture -> Effect Unit
-    render2 ctx pics =
-      traverse_ (renderPicture ctx) pics
+    renderWorld :: Context2D -> State -> Effect Unit
+    renderWorld ctx world =
+      traverse_ (renderCell ctx) $ cellsWithCoordinates world
 
-    renderPicture :: Context2D -> Picture -> Effect Unit
-    renderPicture ctx = case _ of
-      Rect opt -> do
+    renderCell :: Context2D -> WithCoord Cell -> Effect Unit
+    renderCell ctx { coord, cell } = case cell of
+      Empty -> pure unit
+
+      Concrete -> do
+        let rect = coordToRect coord
         GCanvas.setFillStyle ctx "#000"
-        GCanvas.fillRect ctx opt
-
-      Circle { x, y, radius } -> do
-        GCanvas.arc ctx {x, y, radius, start : zero, end : pi * 2.0, useCounterClockwise : false }
-        GCanvas.stroke ctx
+        GCanvas.fillRect ctx rect
 
     onResize :: Vec D2 Number -> Context2D -> Effect Context2D
     onResize _size ctx =
       pure ctx
 
 handleAction :: forall cs o m. MonadEffect m => Action → H.HalogenM State Action cs o m Unit
-handleAction Increment = H.modify_ \st -> st { count = st.count + 1 }
 handleAction (MouseMove e) = do
-  liftEffect $ Console.log msg
-  where
-    msg = "Mouse moved. Client: "
-          <> (show $ Mouse.clientX e)
-          <> ", "
-          <> (show $ Mouse.clientY e)
-          <> ". Screen: "
-          <> (show $ Mouse.screenX e)
-          <> ", "
-          <> (show $ Mouse.screenY e)
-          <> ". Offset: "
-          <> (show $ Mouse.offsetX e)
-          <> ", "
-          <> (show $ Mouse.offsetY e)
+  if Mouse.altKey e then
+    let coord = mousePosToCoord e in
+    H.modify_ \state -> setCell coord Concrete state
+  else
+    pure unit
