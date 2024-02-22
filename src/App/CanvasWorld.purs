73fabe324acd67e8629953c8b576443e6311bbd5
Mikhail Brinchuk <thecentury@gmail.com>
2024-02-23 00:04:12 +0100

    Cells can fall

--- src/App/CanvasWorld.purs
+++ src/App/CanvasWorld.purs
@@ -7,11 +7,19 @@ import CSS (border, px, solid)
 import CSS.Geometry (height, width) as CSS
 import Color (black)
 import Control.Monad.Rec.Class (forever)
+import Control.Monad.ST (ST)
+import Control.Monad.ST as ST
+import Control.Monad.ST.Ref (STRef)
+import Control.Monad.ST.Ref as STRef
 import Data.Array as Array
+import Data.Array.ST (STArray)
+import Data.Array.ST as STA
+import Data.Array.ST as STArray
 import Data.Int (rem)
 import Data.Int as Int
 import Data.List as List
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(Tuple))
 import Data.Typelevel.Num (D2)
 import Data.Vec (Vec, vec2)
 import Effect (Effect)
@@ -20,7 +28,7 @@ import Effect.Aff (Milliseconds(..))
 import Effect.Aff (delay, forkAff) as Aff
 import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect, liftEffect)
-import Effect.Console as Console
+--import Effect.Console as Console
 import Graphics.Canvas (Context2D)
 import Graphics.Canvas as GCanvas
 import Halogen as H
@@ -49,18 +57,66 @@ type Coord = { x :: Int, y :: Int }
 
 type WithCoord a = { coord :: Coord, cell :: a }
 
+attachCoord :: forall a . Coord -> a -> WithCoord a
+attachCoord coord cell = { coord, cell }
+
+data Generation a =
+    Current a
+  | Next a
+
+promoteGeneration :: forall a . Generation a -> Generation a
+promoteGeneration (Current a) = Next a
+promoteGeneration (Next a) = Next a
+
+withoutGeneration :: forall a . Generation a -> a
+withoutGeneration (Current a) = a
+withoutGeneration (Next a) = a
+
+data Neighbour =
+    TopLeft
+  | Top
+  | TopRight
+  | Left
+  | Right
+  | BottomLeft
+  | Bottom
+  | BottomRight
+
+neighbourCoord :: Coord -> Neighbour -> Coord
+neighbourCoord { x, y } = case _ of
+  TopLeft -> { x: x - 1, y: y - 1 }
+  Top -> { x, y: y - 1 }
+  TopRight -> { x: x + 1, y: y - 1 }
+  Left -> { x: x - 1, y }
+  Right -> { x: x + 1, y }
+  BottomLeft -> { x: x - 1, y: y + 1 }
+  Bottom -> { x, y: y + 1 }
+  BottomRight -> { x: x + 1, y: y + 1 }
+
+neighbour :: forall a . Array a -> Coord -> Neighbour -> Maybe a
+neighbour cells coord n =
+  let
+    coord' = neighbourCoord coord n
+  in
+    Array.index cells (coordIndex coord')
+
+neighbourMut :: forall h a . STArray h a -> Coord -> Neighbour -> ST h (Maybe a)
+neighbourMut cells coord n = do
+  let coord' = neighbourCoord coord n
+  peek coord' cells
+
 coordIndex :: Coord -> Int
 coordIndex { x, y } = y * worldWidth + x
 
 indexToCoord :: Int -> Coord
 indexToCoord i = { x: i `rem` worldWidth, y: i / worldWidth }
 
-cellsWithCoordinates :: State -> Array (WithCoord Cell)
-cellsWithCoordinates world =
-  Array.mapWithIndex (\i cell -> { coord: indexToCoord i, cell }) world.cells
+cellsWithCoordinates :: forall a . Array a -> Array (WithCoord a)
+cellsWithCoordinates cells =
+  Array.mapWithIndex (\i cell -> attachCoord (indexToCoord i) cell) cells
 
-cellAt :: Coord -> State -> Maybe Cell
-cellAt coord world = Array.index world.cells (coordIndex coord)
+cellAt :: Coord -> Array Cell -> Maybe Cell
+cellAt coord cells = Array.index cells (coordIndex coord)
 
 setCell :: Coord -> Cell -> State -> State
 setCell coord cell world = world { cells = fromMaybe world.cells $ Array.updateAt (coordIndex coord) cell world.cells }
@@ -157,11 +213,14 @@ renderer =
 
     renderWorld :: Context2D -> State -> Effect Unit
     renderWorld ctx world =
-      Effect.foreachE (cellsWithCoordinates world) $ renderCell ctx
+      Effect.foreachE (cellsWithCoordinates world.cells) $ renderCell ctx
 
     renderCell :: Context2D -> WithCoord Cell -> Effect Unit
     renderCell ctx { coord, cell } = case cell of
-      Empty -> pure unit
+      Empty -> do
+        let rect = coordToRect coord
+        GCanvas.setFillStyle ctx "#FFF"
+        GCanvas.fillRect ctx rect
 
       Concrete -> do
         let rect = coordToRect coord
@@ -184,7 +243,8 @@ handleAction (MouseMove e) = do
   else
     pure unit
 
-handleAction Tick = pure unit
+handleAction Tick =
+  H.modify_ \state -> state { cells = updateWorld state.cells }
 
 timer :: forall m a. MonadAff m => a -> m (HS.Emitter a)
 timer val = do
@@ -193,3 +253,104 @@ timer val = do
     Aff.delay $ Milliseconds 200.0
     H.liftEffect $ HS.notify listener val
   pure emitter
+
+peek :: forall h a
+           . Coord
+          -> STArray h a
+          -> ST h (Maybe a)
+peek coord cells = do
+  let i = coordIndex coord
+  STArray.peek i cells
+
+set :: forall h a
+          . Coord
+          -> a
+          -> STArray h a
+          -> ST h Unit
+set coord cell cells = do
+  let i = coordIndex coord
+  _ <- STArray.poke i cell cells
+  pure unit
+
+exchangeF :: forall h a . Coord -> Neighbour -> (a -> a) -> STArray h a -> ST h Unit
+exchangeF thisCoord n f cells = do
+  let nCoord = neighbourCoord thisCoord n
+  thisCell <- peek thisCoord cells
+  otherCell <- peek nCoord cells
+  case Tuple thisCell otherCell of
+    Tuple (Just this) (Just other) -> do
+      set nCoord (f this) cells
+      set thisCoord (f other) cells
+    _ -> pure unit
+
+updateCell :: forall h . WithCoord (Generation Cell) -> STArray h (Generation Cell) -> ST h Unit
+updateCell { coord, cell } cells = do
+  case cell of
+    Current Empty -> pure unit
+    Current Concrete -> do
+      bottom <- neighbourMut cells coord Bottom
+      case withoutGeneration <$> bottom of
+        Just Empty ->
+          exchangeF coord Bottom promoteGeneration cells
+        Just Concrete -> pure unit
+        Nothing -> pure unit
+    -- Do not update already updated cells.
+    Next _ -> pure unit
+
+-- | This type provides a slightly easier way of iterating over an array's
+-- | elements in an STArray computation, without having to keep track of
+-- | indices.
+data Iterator r a = Iterator (Int -> ST r (Maybe a)) (STRef r Int)
+
+-- | Make an Iterator given an indexing function into an array (or anything
+-- | else). If `xs :: Array a`, the standard way to create an iterator over
+-- | `xs` is to use `iterator (xs !! _)`, where `(!!)` comes from `Data.Array`.
+iterator :: forall r a. (Int -> ST r (Maybe a)) -> ST r (Iterator r a)
+iterator f =
+  Iterator f <$> STRef.new 0
+
+-- | Perform an action once for each item left in an iterator. If the action
+-- | itself also advances the same iterator, `iterate` will miss those items
+-- | out.
+iterate :: forall r a. Iterator r a -> (a -> ST r Unit) -> ST r Unit
+iterate iter f = do
+  break <- STRef.new false
+  ST.while (not <$> STRef.read break) do
+    mx <- next iter
+    case mx of
+      Just x -> f x
+      Nothing -> void $ STRef.write true break
+
+iterateWithIndex :: forall r a. Iterator r a -> (Int -> a -> ST r Unit) -> ST r Unit
+iterateWithIndex iter f = do
+  break <- STRef.new false
+  ST.while (not <$> STRef.read break) do
+    Tuple index mx <- nextWithIndex iter
+    case mx of
+      Just x -> f index x
+      Nothing -> void $ STRef.write true break
+
+-- | Get the next item out of an iterator, advancing it. Returns Nothing if the
+-- | Iterator is exhausted.
+next :: forall r a. Iterator r a -> ST r (Maybe a)
+next (Iterator f currentIndex) = do
+  i <- STRef.read currentIndex
+  _ <- STRef.modify (_ + 1) currentIndex
+  f i
+
+nextWithIndex :: forall r a. Iterator r a -> ST r (Tuple Int (Maybe a))
+nextWithIndex (Iterator f currentIndex) = do
+  i <- STRef.read currentIndex
+  _ <- STRef.modify (_ + 1) currentIndex
+  element <- f i
+  pure $ Tuple i element
+
+updateWorld :: Array Cell -> Array Cell
+updateWorld cells = withoutGeneration <$> STArray.run do
+  let genCells = Current <$> cells
+  cellsMut <- STArray.thaw genCells
+  i <- iterator (\ix -> STArray.peek ix cellsMut)
+  iterateWithIndex i \ix cell -> do
+    let coord = indexToCoord ix
+    updateCell (attachCoord coord cell) cellsMut
+  pure cellsMut
