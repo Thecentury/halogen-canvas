980a2a2deb4b6544f625a11efb4c1139cbff224d
Mikhail Brinchuk <thecentury@gmail.com>
2024-02-24 12:24:12 +0100

    Cells to their own module

--- src/App/CanvasWorldUI.purs
+++ src/App/CanvasWorldUI.purs
@@ -2,20 +2,16 @@ module App.CanvasWorldUI where
 
 import Prelude
 
-import App.Coordinates (Coord, Neighbour(..), WithCoord, attachCoord, cellsWithCoordinates, coordIndex, heightInPixels, indexToCoord, neighbourMut, neighbourWithCoordMut, pixelSize, set, widthInPixels, worldHeight, worldWidth, exchangeF)
+import App.Cell (Cell(..), updateWorld)
+import App.Coordinates (Coord, WithCoord, cellsWithCoordinates, coordIndex, heightInPixels, pixelSize, widthInPixels, worldHeight, worldWidth)
 import App.MouseEvent (offsetX, offsetY) as Mouse
-import App.MutableArray (iterateReverseWithIndex, iteratorAt, peekWithIndex)
 import CSS (border, px, solid)
 import CSS.Geometry (height, width) as CSS
 import Color (black)
 import Control.Monad.Rec.Class (forever)
-import Control.Monad.ST (ST)
 import Data.Array as Array
-import Data.Array.ST (STArray)
-import Data.Array.ST as STArray
 import Data.Int as Int
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Tuple (Tuple(Tuple))
 import Data.Typelevel.Num (D2)
 import Data.Vec (Vec, vec2)
 import Effect (Effect)
@@ -40,27 +36,9 @@ import Web.HTML.HTMLCanvasElement (HTMLCanvasElement)
 import Web.UIEvent.MouseEvent (MouseEvent)
 import Web.UIEvent.MouseEvent (altKey, shiftKey) as Mouse
 
-data Cell =
-    Empty
-  | Acid
-  | Concrete
-  | FrozenConcrete
-
 type State = {
   cells :: Array Cell
 }
-
-data Generation a =
-    Current a
-  | Next a
-
-promoteGeneration :: forall a . Generation a -> Generation a
-promoteGeneration (Current a) = Next a
-promoteGeneration (Next a) = Next a
-
-withoutGeneration :: forall a . Generation a -> a
-withoutGeneration (Current a) = a
-withoutGeneration (Next a) = a
 
 setCell :: Coord -> Cell -> State -> State
 setCell coord cell world = world { cells = fromMaybe world.cells $ Array.updateAt (coordIndex coord) cell world.cells }
@@ -194,50 +172,3 @@ timer val = do
     Aff.delay $ Milliseconds 100.0
     H.liftEffect $ HS.notify listener val
   pure emitter
-
-updateCell :: forall h . WithCoord (Generation Cell) -> STArray h (Generation Cell) -> ST h Unit
-updateCell { coord, cell } cells = do
-  case cell of
-    Current Empty -> pure unit
-    Current FrozenConcrete -> pure unit
-    Current Acid -> do
-      bottom <- neighbourWithCoordMut cells coord Bottom
-      case (\(Tuple coord' c) -> Tuple coord' (withoutGeneration c)) <$> bottom of
-        Just (Tuple _ Empty) ->
-          exchangeF coord Bottom promoteGeneration cells
-        Just (Tuple nCoord Concrete) -> do
-          set nCoord (Next Acid) cells
-          set coord (Next Empty) cells
-        Just (Tuple _ Acid) -> pure unit
-        Just (Tuple _ FrozenConcrete) -> pure unit
-        Nothing -> pure unit
-
-    Current Concrete -> do
-      bottom <- neighbourMut cells coord Bottom
-      case withoutGeneration <$> bottom of
-        Just Empty ->
-          exchangeF coord Bottom promoteGeneration cells
-        Just _ -> do
-          bottomLeft <- neighbourMut cells coord BottomLeft
-          case withoutGeneration <$> bottomLeft of
-            Just Empty ->
-              exchangeF coord BottomLeft promoteGeneration cells
-            _ -> do
-              bottomRight <- neighbourMut cells coord BottomRight
-              case withoutGeneration <$> bottomRight of
-                Just Empty ->
-                  exchangeF coord BottomRight promoteGeneration cells
-                _ -> pure unit
-        Nothing -> pure unit
-    -- Do not update already updated cells.
-    Next _ -> pure unit
-
-updateWorld :: Array Cell -> Array Cell
-updateWorld cells = withoutGeneration <$> STArray.run do
-  let genCells = Current <$> cells
-  cellsMut <- STArray.thaw genCells
-  i <- iteratorAt (Array.length genCells - 1) $ peekWithIndex cellsMut
-  iterateReverseWithIndex i \(Tuple ix cell) -> do
-    let coord = indexToCoord ix
-    updateCell (attachCoord coord cell) cellsMut
-  pure cellsMut
---
+++ src/App/Cell.purs
@@ -0,0 +1,80 @@
+module App.Cell (
+  Cell(..),
+  updateWorld
+) where
+
+import Prelude
+import App.Coordinates (Neighbour(..), WithCoord, attachCoord, exchangeF, indexToCoord, neighbourMut, neighbourWithCoordMut, set)
+import Data.Tuple (Tuple(Tuple))
+import App.MutableArray (iterateReverseWithIndex, iteratorAt, peekWithIndex)
+import Control.Monad.ST (ST)
+import Control.Monad.ST as ST
+import Data.Array as Array
+import Data.Array.ST (STArray)
+import Data.Array.ST as STArray
+import Data.Maybe (Maybe(..))
+
+data Cell =
+    Empty
+  | Acid
+  | Concrete
+  | FrozenConcrete
+
+data Generation a =
+    Current a
+  | Next a
+
+promoteGeneration :: forall a . Generation a -> Generation a
+promoteGeneration (Current a) = Next a
+promoteGeneration (Next a) = Next a
+
+withoutGeneration :: forall a . Generation a -> a
+withoutGeneration (Current a) = a
+withoutGeneration (Next a) = a
+
+updateCell :: forall h . WithCoord (Generation Cell) -> STArray h (Generation Cell) -> ST h Unit
+updateCell { coord, cell } cells = do
+  case cell of
+    Current Empty -> pure unit
+    Current FrozenConcrete -> pure unit
+    Current Acid -> do
+      bottom <- neighbourWithCoordMut cells coord Bottom
+      case (\(Tuple coord' c) -> Tuple coord' (withoutGeneration c)) <$> bottom of
+        Just (Tuple _ Empty) ->
+          exchangeF coord Bottom promoteGeneration cells
+        Just (Tuple nCoord Concrete) -> do
+          set nCoord (Next Acid) cells
+          set coord (Next Empty) cells
+        Just (Tuple _ Acid) -> pure unit
+        Just (Tuple _ FrozenConcrete) -> pure unit
+        Nothing -> pure unit
+
+    Current Concrete -> do
+      bottom <- neighbourMut cells coord Bottom
+      case withoutGeneration <$> bottom of
+        Just Empty ->
+          exchangeF coord Bottom promoteGeneration cells
+        Just _ -> do
+          bottomLeft <- neighbourMut cells coord BottomLeft
+          case withoutGeneration <$> bottomLeft of
+            Just Empty ->
+              exchangeF coord BottomLeft promoteGeneration cells
+            _ -> do
+              bottomRight <- neighbourMut cells coord BottomRight
+              case withoutGeneration <$> bottomRight of
+                Just Empty ->
+                  exchangeF coord BottomRight promoteGeneration cells
+                _ -> pure unit
+        Nothing -> pure unit
+    -- Do not update already updated cells.
+    Next _ -> pure unit
+
+updateWorld :: Array Cell -> Array Cell
+updateWorld cells = withoutGeneration <$> STArray.run do
+  let genCells = Current <$> cells
+  cellsMut <- STArray.thaw genCells
+  i <- iteratorAt (Array.length genCells - 1) $ peekWithIndex cellsMut
+  iterateReverseWithIndex i \(Tuple ix cell) -> do
+    let coord = indexToCoord ix
+    updateCell (attachCoord coord cell) cellsMut
+  pure cellsMut
